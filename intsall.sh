8b1f4aa82f15245a0c851c51488f1a7fd0432635
opmm0809 <opmm0809@gmail.com>
2025-09-27 15:41:16 +0800

    Add tmux conf

--- intsall.sh
+++ intsall.sh
@@ -2,8 +2,24 @@
 
 MY_DOTFILES_ROOT=$(realpath $(dirname $0))
 
+# create a symbol link
+# if there is a file exist backup the file
+create_symlink() {
+    file=$1
+    link=$2
+
+    if [[ -e "$link" ]]; then
+        mv $link ${link}.backup
+    fi
+
+    ln -s $file $link
+}
+
+# Setup for neovim
 mkdir -p ~/.config
 mkdir -p ~/.local/share/nvim
-ln -s ${MY_DOTFILES_ROOT}/vim/nvim ~/.config/nvim
-ln -s ${MY_DOTFILES_ROOT}/vim/site ~/.local/share/nvim/site
+create_symlink ${MY_DOTFILES_ROOT}/vim/nvim ~/.config/nvim
+create_symlink ${MY_DOTFILES_ROOT}/vim/site ~/.local/share/nvim/site
 
+# Setup for tmux
+create_symlink ${MY_DOTFILES_ROOT}/tmux/tmux.conf ~/.tmux.conf
