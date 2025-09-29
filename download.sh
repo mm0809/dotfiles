22a147502dd621bbaa565c29068089f1ed84f884
mm0809 <opmm0809@gmail.com>
2025-09-30 00:45:41 +0800

    Add fzf and fzf plugin

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 mkdir -p downloads
 wget -P downloads/ https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz
-wget -p doenloads/ https://github.com/clangd/clangd/releases/download/20.1.8/clangd-linux-20.1.8.zip
+wget -p downloads/ https://github.com/clangd/clangd/releases/download/20.1.8/clangd-linux-20.1.8.zip
+wget -p downloads/ https://github.com/junegunn/fzf/releases/download/v0.65.2/fzf-0.65.2-linux_amd64.tar.gz
