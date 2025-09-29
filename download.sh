45870cd4725d127708b4e2c488d1d9dbac575694
mm0809 <opmm0809@gmail.com>
2025-09-29 23:51:05 +0800

    Add download script for nvim and clangd

---
+++ download.sh
@@ -0,0 +1,3 @@
+mkdir -p downloads
+wget -P downloads/ https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz
+wget -p doenloads/ https://github.com/clangd/clangd/releases/download/20.1.8/clangd-linux-20.1.8.zip
