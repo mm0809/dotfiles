b5b9f0acb62c324700aa407d2a8b7d96702e9268
mm0809 <opmm0809@gmail.com>
2025-12-10 23:52:02 +0800

    Add fzf.vim

--- vim/nvim/lua/lsp.lua
+++ vim/nvim/lua/lsp.lua
@@ -39,7 +39,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback =function(event)
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client:supports_method('textDocument/completion') then
-            vim.print("hellpo")
             vim.lsp.completion.enable(true, client.id, event.buf, {autotrigger = true})
         end
 
