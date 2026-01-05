54405a5617ff9e296889b57c30028b863d9f3dbb
mm0809 <opmm0809@gmail.com>
2026-01-05 19:21:55 +0800

    Add lua_ls language server configuration
    - Configure lua_ls with Neovim runtime and workspace settings
    - Add library path for LuaJIT and Neovim runtime files
    - Enable code lens and hints for Lua
    - Fix LSP client nil check in LspAttach handler

--- vim/nvim/lua/lsp.lua
+++ vim/nvim/lua/lsp.lua
@@ -30,15 +30,82 @@ vim.lsp.config['pyright'] = {
     },
 }
 
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library'
+          -- '${3rd}/busted/library'
+        }
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = {
+        --   vim.api.nvim_get_runtime_file('', true),
+        -- }
+      }
+    })
+  end,
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = {
+    '.emmyrc.json',
+    '.luarc.json',
+    '.luarc.jsonc',
+    '.luacheckrc',
+    '.stylua.toml',
+    'stylua.toml',
+    'selene.toml',
+    'selene.yml',
+    '.git',
+  },
+  settings = {
+    Lua = {
+      codeLens = { enable = true },
+      hint = { enable = true, semicolon = 'Disable' },
+    },
+  }
+})
+
+
 vim.lsp.enable('pyright')
 vim.lsp.enable('clangd')
+vim.lsp.enable('lua_ls')
 
 -- LSP: keybindings
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = "Lsp keybindings",
     callback =function(event)
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client:supports_method('textDocument/completion') then
+        if client ~= nil and client:supports_method('textDocument/completion') then
             vim.lsp.completion.enable(true, client.id, event.buf, {autotrigger = true})
         end
 
@@ -58,7 +125,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 -- LSP: set diagnostic messages
-vim.diagnostic.config({ 
+vim.diagnostic.config({
     virtual_text = false,
     update_in_insert = false
 })
